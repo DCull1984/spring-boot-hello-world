d083e7867c93b1ab05f70d335e2ed9971412f8b9
DCull1984 <nowe2111@yahoo.co.uk>
2019-01-12 13:23:38 +0000

    Update deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -2,8 +2,8 @@
 #changed
 project=${PWD##*/}
 author=bobcrutchley
-host_port=8080
-container_port=8080
+host_port=9000
+container_port=9000
 mvn clean package
 docker stop ${project}
 docker rm ${project}
